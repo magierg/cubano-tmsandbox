6772fe16d96d3129909bd862b39db650b495d9a7
jkemp002 <jkemp002@P1130051191.corp.ssi.govt.nz>
2018-03-29 10:46:22 +1300

    Initial push of a template project to support development with Cubano.

---
+++ src/test/java/org/concordion/cubano/template/specification/google/Google.java
@@ -0,0 +1,7 @@
+package org.concordion.cubano.template.specification.google;
+
+import org.concordion.cubano.template.specification.ConcordionIndex;
+
+public class Google extends ConcordionIndex {
+
+}
